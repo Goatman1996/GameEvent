97c34b8adcf0c5cf51233fbd117839ac36369fb4
廖小白 <liaoxiaobai@liaoxiaobaideMacBook-Pro.local>
2024-05-06 16:00:26 +0800

    update

--- Runtime/IGameEvent.cs
+++ Runtime/IGameEvent.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading.Tasks;
 
 namespace GameEvent
 {
@@ -10,5 +11,29 @@ namespace GameEvent
     public interface IGameTask
     {
 
+    }
+
+    public class GameEvent<T> where T : IGameEvent
+    {
+        public static event Action<T> Event;
+
+        internal void Invoke(T arg)
+        {
+            Event?.Invoke(arg);
+        }
+    }
+
+    public class GameTask<T> where T : IGameTask
+    {
+        public static event Func<T, Task> Event;
+
+        internal async Task InvokeAsync(T arg)
+        {
+            foreach (var invoker in Event.GetInvocationList())
+            {
+                var task = (Task)invoker?.DynamicInvoke(arg);
+                await task;
+            }
+        }
     }
 }
